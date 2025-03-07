22ee4b137fac28879965f2eba2dfd7e8395d4d6d
nadiarojas <rojassnaa970@gmail.com>
2025-03-06 21:13:50 -0300

    Revert "Update Authenticate.php"
    This reverts commit 6a0406b96ce54255b8223fa7c3b88cb0dec119a6.

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -12,14 +12,6 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
-        // Si la solicitud espera una respuesta JSON, devuelve null
-        if ($request->expectsJson()) {
-            return null;
-        }
-
-        // Si no, devuelve una respuesta JSON con un mensaje de error
-        return response()->json([
-            'message' => 'No autenticado.',
-        ], 401); // Código de estado HTTP 401: No autorizado
+        return $request->expectsJson() ? null : route('login');
     }
 }
