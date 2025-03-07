a71f24fe1b5fbe722370349f750cb7c620b4c269
nadiarojas <rojassnaa970@gmail.com>
2025-03-06 21:13:25 -0300

    Revert "Sanctum"
    This reverts commit c1046487db422f04b4f4ee93bd254fcadc9d5685.

--- config/auth.php
+++ config/auth.php
@@ -31,7 +31,7 @@ return [
     | users are actually retrieved out of your database or other storage
     | mechanisms used by this application to persist your user's data.
     |
-    | Supported: "session", "sanctum"
+    | Supported: "session"
     |
     */
 
@@ -39,11 +39,6 @@ return [
         'web' => [
             'driver' => 'session',
             'provider' => 'users',
-        ],
-        'api' => [ // Agregado para Sanctum
-            'driver' => 'sanctum',
-            'provider' => 'users',
-            'hash' => false,
         ],
     ],
 
@@ -67,8 +62,13 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\Models\Usuario::class,
+            'model' => App\Models\User::class,
         ],
+
+        // 'users' => [
+        //     'driver' => 'database',
+        //     'table' => 'users',
+        // ],
     ],
 
     /*
--- database/migrations/2025_03_03_175625_create_tarea_usuarios_table.php
+++ database/migrations/2025_03_03_175625_create_tarea_usuarios_table.php
@@ -7,11 +7,10 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
     public function up() {
         Schema::create('tarea_usuarios', function (Blueprint $table) {
-            $table->id(); // Clave primaria autoincremental
             $table->foreignId('tarea_id')->constrained('tareas')->onDelete('cascade');
             $table->foreignId('usuario_id')->constrained('usuarios')->onDelete('cascade');
             $table->boolean('completado')->default(false);
-            $table->unique(['tarea_id', 'usuario_id']); // Restricción única para evitar duplicados
+            $table->primary(['tarea_id', 'usuario_id']);
         });
     }
 
